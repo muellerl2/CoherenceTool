2049f457e221563f766f39d213d75227a32b37ba
Geoffrey Mo <geoffrey.mo@ligo.org>
2018-06-20 07:32:53 -0700

    moved coherence output folder to ~/public_html/coherences/ and updated instructions

--- line_search.sh
+++ line_search.sh
@@ -3,11 +3,19 @@
 
 # run the matlab main.m file
 cat main.m | matlab -nodesktop -nosplash
+
 # generate html from python script
-pyoutput=$( python LineExp.py ~/output_buffer )
+pyoutput=$( python LineExp.py ~/CoherenceTool/output_buffer )
+
+# create coherences directory in ~/public_html
+mkdir -p ~/public_html/coherences/
+
 # take output of html and paste it into the correct line in index.html
-sed -i "0,/ul\>/  s@.*ul\>.*@&\n$pyoutput@" ~/public_html/index.html 
+sed -i "0,/ul\>/  s@.*ul\>.*@&\n$pyoutput@" ~/public_html/coherences/index.html 
+
 # copy outputs from matlab script to public_html folder for website
-cp -r ~/output_buffer/* ~/public_html/ 
+cp -r ~/CoherenceTool/output_buffer/* ~/public_html/coherences/
+rm -rf ~/CoherenceTool/output_buffer
+
 # done
 echo "Job complete"
